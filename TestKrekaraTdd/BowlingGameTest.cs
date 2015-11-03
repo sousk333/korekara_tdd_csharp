16746b28e3e17ec12fdd122051868e3492eee90c
sousk333 <fire.bluestar@gmail.com>
2015-11-03 15:52:58 +0900

    Init

---
+++ TestKrekaraTdd/BowlingGameTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using KorekaraTdd;
+
+namespace TestKrekaraTdd
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BowlingGameTest
+    {
+        [Test]
+        public void インスタンス化のテスト()
+        {
+            var game = new BowlingGame();
+            Assert.That(game, Is.Not.Null);
+        }
+    }
+}
